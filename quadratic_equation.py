382df68674632fbe34cd8649527283c0bbde247e
unknown <ProkhorenkovSV@sergeycomp.Sibdorstroy.local>
2018-02-12 11:43:24 +0500

    исправлен импорт math, добавлено существование комплексных корней

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,7 +5,9 @@ def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant < 0:
-        return None, None
-    else:
+    if discriminant == 0:
+        return root1, None
+    elif discriminant > 0:
         return root1, root2
+    else:
+        return None, None
